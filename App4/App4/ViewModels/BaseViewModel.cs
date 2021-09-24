bd8a80d666acaeaa541112429bcd18c53c774514
SaidPereyra <zaidpereyra@gmail.com>
2021-09-23 22:58:54 -0500

    Agregar archivos de proyecto.

---
+++ App4/App4/ViewModels/BaseViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace App4.ViewModels
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string propertyName = null)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
---
+++ App4/App4/ViewModels/CountViewModel.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace App4.ViewModels
+{
+    public class CountViewModel : BaseViewModel
+    {
+        int _count;
+        ICommand _buttonClickCommand;
+        ICommand _buttonResetCommand;
+        string _countConverted;
+
+        public CountViewModel()
+        {
+            _count = 0;
+        }
+
+        public int Contador
+        {
+            get => _count;
+            set
+            {
+                if (value == _count) return;
+                {
+                    _count = value;
+                    CountConverter = $"Has dado click {_count} veces";
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public string CountConverter
+        {
+            get => _countConverted;
+            set
+            {
+                if (string.Equals(_countConverted, value)) return;
+                _countConverted = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand ButtonClickCommand
+        {
+            get
+            {
+                if (_buttonClickCommand == null)
+                 _buttonClickCommand = new Command(ButtonClickAction);
+                return _buttonClickCommand;
+            }
+        }
+
+        private void ButtonClickAction()
+        {
+            Contador++;
+        }
+
+        public ICommand ButtonResetCommand
+        {
+            get
+            {
+                if (_buttonResetCommand == null)
+                    _buttonResetCommand = new Command(ButtonResetAction);
+                return _buttonResetCommand;
+            }
+        }
+        private void ButtonResetAction()
+        {
+            CountConverter = "Resetado pa";
+            _count = 0;
+        }
+    }
+}
